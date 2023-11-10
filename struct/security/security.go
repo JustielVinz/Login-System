a6b387cc590e9fc7c5b71449fa80b76f04d025fc
FDSAP-JVVELASCO <fdsap-jc.volasco@FDSAP-JVVELASCO.local>
2023-11-10 15:54:19 +0800

    new security in struct

--- struct/security/security.go
+++ struct/security/security.go
@@ -5,3 +5,10 @@ import "github.com/golang-jwt/jwt"
 type Claims struct {
 	jwt.StandardClaims
 }
+
+type User struct {
+	ID       int    `json:"id"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+	Status   string `json:"status"`
+}
