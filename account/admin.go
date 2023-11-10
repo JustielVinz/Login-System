4c2587ca3e7e11068c0b0449dd0d8d68e624a4e6
FDSAP-JVVELASCO <fdsap-jc.volasco@FDSAP-JVVELASCO.local>
2023-11-10 15:52:44 +0800

    JWT handlers

---
+++ account/admin.go
@@ -0,0 +1,40 @@
+package account
+
+import (
+	"sample/middleware/utils/database"
+	errors "sample/struct/error"
+	"sample/struct/security"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func RegisterHandler(c *fiber.Ctx) error {
+	admin := security.User{}
+	// Get the current time
+	currentTime := time.Now()
+	// Format and print the current time
+	Exactime := currentTime.Format("2006-01-02 15:04:05")
+
+	// Parse JSON request body
+	if err := c.BodyParser(&admin); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Invalid request body"})
+	}
+
+	// Hash the password before storing it
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(admin.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Failed to hash password"})
+	}
+
+	// Insert the user into the database
+	if err := database.DBConn.Debug().Raw("INSERT INTO credentials (id, username, password, status, created_at) VALUES (?,?, ?, ?, ?)", admin.ID, admin.Username, hashedPassword, admin.Status, Exactime).Scan(&admin).Error; err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(errors.ErrorModel{
+			Message:   "Error in inserting user data",
+			IsSuccess: false,
+		})
+	}
+
+	return c.Status(fiber.StatusCreated).JSON(fiber.Map{"message": "User registered successfully"})
+}
